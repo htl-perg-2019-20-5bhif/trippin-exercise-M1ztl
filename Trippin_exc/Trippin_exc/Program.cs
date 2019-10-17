6c11740b593e177972e2f5332d9b8194af18bd0a
M1ztl <lukas.mistlberger1@gmail.com>
2019-10-17 07:56:39 +0200

    homework

---
+++ Trippin_exc/Trippin_exc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace Trippin_exc
+{
+    class Program
+    {
+        static HttpClient HttpClient = new HttpClient() { BaseAddress = new Uri("https://services.odata.org/TripPinRESTierService/(S(5dhay5wvj2x3o5h3oh1menja))/") };
+        
+        static async Task Main(string[] args)
+        {
+
+            var readFile = await File.ReadAllTextAsync("users.json");
+            IEnumerable<TransferObejcts.User> users = JsonSerializer.Deserialize<IEnumerable<TransferObejcts.User>>(readFile);
+
+            foreach (var user in users)
+            {
+                var response = await HttpClient.GetAsync("People('" + user.UserName + "')");
+                if (!response.IsSuccessStatusCode)
+                {
+                    var newUser = new StringContent(JsonSerializer.Serialize(new TransferObejcts.Tripping(user)), Encoding.UTF8, "application/json");
+                    await HttpClient.PostAsync("People", newUser);
+                    Console.WriteLine("User inserted:"+user.UserName+" " +user.LastName);
+                }
+                else
+                {
+                    Console.WriteLine("Found user" + user.UserName);
+                }
+            }
+        }
+    }
+}
---
+++ Trippin_exc/Trippin_exc/TransferObejcts.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Trippin_exc
+{
+    class TransferObejcts
+    {
+        public class User
+        {
+            [JsonPropertyName("UserName")]
+            public string UserName { get; set; }
+
+            [JsonPropertyName("FirstName")]
+            public string FirstName { get; set; }
+
+            [JsonPropertyName("LastName")]
+            public string LastName { get; set; }
+            [JsonPropertyName("Email")]
+            public string Email { get; set; }
+
+            [JsonPropertyName("Address")]
+            public string Address { get; set; }
+
+            [JsonPropertyName("CityName")]
+            public string City { get; set; }
+
+            [JsonPropertyName("Country")]
+            public string Country { get; set; }
+        }
+        public class AddressInfo
+        {
+            [JsonPropertyName("Address")]
+            public string Address { get; set; }
+
+            [JsonPropertyName("City")]
+            public City City { get; set; }
+
+            public AddressInfo(string Address, City City)
+            {
+                this.Address = Address;
+                this.City = City;
+            }
+        }
+
+        public class City
+        {
+            [JsonPropertyName("Name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("CountryRegion")]
+            public string CountryRegion { get; set; }
+
+            [JsonPropertyName("Region")]
+            public string Region { get; set; }
+
+            public City(string Name, string CountryRegion, string Region)
+            {
+                this.Name = Name;
+                this.CountryRegion = CountryRegion;
+                this.Region = Region;
+            }
+        }
+        public class Tripping
+        {
+            [JsonPropertyName("UserName")]
+            public string UserName { get; set; }
+
+            [JsonPropertyName("FirstName")]
+            public string FirstName { get; set; }
+
+            [JsonPropertyName("LastName")]
+            public string LastName { get; set; }
+
+            [JsonPropertyName("Emails")]
+            public IEnumerable<string> Emails { get; set; }
+
+            [JsonPropertyName("AddressInfo")]
+            public IEnumerable<AddressInfo> AddressInfo { get; set; }
+
+            public Tripping(User user)
+            {
+                UserName = user.UserName;
+                FirstName = user.FirstName;
+                LastName = user.LastName;
+
+                Emails = new List<string>() { user.Email };
+
+                AddressInfo = new List<AddressInfo>()
+                { new AddressInfo(user.Address, new City(user.City, user.Country, "")) };
+            }
+
+        }
+
+    }
+}
